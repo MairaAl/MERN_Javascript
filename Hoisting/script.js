1fda568b364f4484aa6117edf894b9044b87ef4a
Maira Al <122545402+MairaAl@users.noreply.github.com>
2024-02-14 11:16:56 -0300

    Added Hoisting file

---
+++ Hoisting/script.js
@@ -0,0 +1,37 @@
+//EJEMPLO 1
+console.log(hello);
+var hello = "world";
+//EJEMPLO 2
+var needle = "haystack";
+test();
+function test() {
+  var needle = "magnet";
+  console.log(needle);
+}
+//EJEMPLO 3
+var brendan = "super cool";
+function print() {
+  brendan = "only okay";
+  console.log(brendan);
+}
+console.log(brendan);
+//EJEMPLO 4
+var food = "chicken";
+console.log(food);
+eat();
+function eat() {
+  food = "half-chicken";
+  console.log(food);
+  var food = "gone";
+}
+//EJEMPLO 5
+mean();
+console.log(food);
+var mean = function () {
+  food = "chicken";
+  console.log(food);
+  var food = "fish";
+  console.log(food);
+};
+console.log(food);
+//EJEMPLO 6
